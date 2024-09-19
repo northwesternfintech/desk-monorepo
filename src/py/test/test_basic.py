903d27c58f54037bfe759f6e564e73f3e1b123d5
John Hileman <106003582+jhileman07@users.noreply.github.com>
2024-09-19 00:16:10 -0500

    adding packaging

--- src/py/test/test_basic.py
+++ src/py/test/test_basic.py
@@ -1,5 +1,8 @@
 import pytest
-
+from src.py.util.hello_test_function import hello_world
 
 def test_basic() -> None:
     assert 4 == 4
+
+def test_hello_world() -> None:
+    assert hello_world() == "Hello, World!"
