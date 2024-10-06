a6693d4db47a8bfb2c2b032d85e0e8bcfa1392f3
Ethan Havemann <echavemann@gmail.com>
2024-10-06 16:09:53 -0500

    >

--- src/pysrc/exec/action.py
+++ src/pysrc/exec/action.py
@@ -1,6 +1,8 @@
 from pysrc.util.types import OrderSide, LiquidityType
 from pysrc.util.instrument import Instrument
 
+from typing import Optional
+
 
 class Action:
     def __init__(
@@ -8,11 +10,13 @@ class Action:
         instrument: Instrument,
         side: OrderSide,
         type: LiquidityType,
-        price: float,
         quantity: float,
+        price: Optional[float],
     ):
         self.instrument = instrument
         self.side = side
         self.liquidity_type = type
-        self.price = price
         self.quantity = quantity
+        self.price: Optional[float] = price
+        if type == LiquidityType.MAKER:
+            assert self.price is not None
