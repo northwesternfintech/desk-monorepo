768e87bbd60e6e23de51d95f19f1fc620416b56c
Ethan Havemann <echavemann@gmail.com>
2024-10-06 16:03:11 -0500

    action

--- src/pysrc/exec/action.py
+++ src/pysrc/exec/action.py
@@ -1,5 +1,12 @@
 from pysrc.util.types import OrderSide, LiquidityType
+from pysrc.util.instrument import Instrument
+
 
 class Action:
 
-    def __init__(self, )
+    def __init__(self, instrument: Instrument, side: OrderSide, type: LiquidityType, price: float, quantity: float):
+        self.instrument = instrument
+        self.side = side
+        self.liquidity_type = type
+        self.price = price
+        self.quantity = quantity
