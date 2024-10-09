8718187c450fcc7c9c754e367c81393eb173cd90
Ethan Havemann <echavemann@gmail.com>
2024-10-08 20:58:15 -0500

    >

--- src/pysrc/test/exec/test_action.py
+++ src/pysrc/test/exec/test_action.py
@@ -5,15 +5,15 @@ from pysrc.util.types import OrderSide, LiquidityType
 import pytest
 
 
-def test_action_ctor_taker():
+def test_action_ctor_taker() -> None:
     Action(get_test_instrument(), OrderSide.BID, LiquidityType.TAKER, 15, 22.1)
     Action(get_test_instrument(), OrderSide.BID, LiquidityType.TAKER, 15, None)
 
 
-def test_action_ctor_maker_good():
+def test_action_ctor_maker_good() -> None:
     Action(get_test_instrument(), OrderSide.BID, LiquidityType.MAKER, 15, 15.6)
 
 
-def test_action_ctor_maker_no_price_failure():
+def test_action_ctor_maker_no_price_failure() -> None:
     with pytest.raises(AssertionError):
         Action(get_test_instrument(), OrderSide.BID, LiquidityType.MAKER, 15, None)
