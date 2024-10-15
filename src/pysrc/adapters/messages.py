7e8021ed190ac92e157670e5e737f8f46e3adab1
Ethan Havemann <echavemann@gmail.com>
2024-10-14 21:24:22 -0500

    >'

--- src/pysrc/adapters/messages.py
+++ src/pysrc/adapters/messages.py
@@ -2,30 +2,45 @@ from pysrc.util.types import Market, OrderSide
 
 
 class SnapshotMessage:
-    def __init__(self, time: int, feedcode: str, bids: list[list[str]], asks: list[list[str]], market: Market):
+    def __init__(
+        self,
+        time: int,
+        feedcode: str,
+        bids: list[list[str]],
+        asks: list[list[str]],
+        market: Market,
+    ):
         self.time = time
         self.feedcode = feedcode
-        self.bids: list[tuple[float]] = []
-        self.asks: list[tuple[float]] = []
+        self.bids: list[tuple[float, float]] = []
+        self.asks: list[tuple[float, float]] = []
         for price, volume in bids:
-            volume = float(volume)
-            if volume != 0.0:
-                self.bids.append(float(price), volume)
+            volume_float = float(volume)
+            if volume_float != 0.0:
+                self.bids.append((float(price), volume_float))
 
         for price, volume in asks:
-            volume = float(volume)
-            if volume != 0.0:
-                self.asks.append(float(price), volume)
+            volume_float = float(volume)
+            if volume_float != 0.0:
+                self.asks.append((float(price), volume_float))
 
-    def get_bids(self) -> list[tuple[float]]:
+    def get_bids(self) -> list[tuple[float, float]]:
         return self.bids
 
-    def get_asks(self) -> list[tuple[float]]:
+    def get_asks(self) -> list[tuple[float, float]]:
         return self.asks
 
 
 class TradeMessage:
-    def __init__(self, time: int, feedcode: str, quantity: int, n_trades: int, side: OrderSide, market: Market):
+    def __init__(
+        self,
+        time: int,
+        feedcode: str,
+        quantity: int,
+        n_trades: int,
+        side: OrderSide,
+        market: Market,
+    ):
         self.time = time
         self.feecode = feedcode
         self.quantity = quantity
