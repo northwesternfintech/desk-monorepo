edbe80ef7725db43635bcb6083ca6bd43b8691a5
Aidan Villasenor <avillasenor@black.tailf7f8d.ts.net>
2024-10-19 21:03:41 -0500

    added unit test for conversion

---
+++ src/pysrc/test/unit/kraken/test_kraken_asset_mappings.py
@@ -0,0 +1,7 @@
+from pysrc.adapters.kraken.asset_mappings import asset_to_kraken, kraken_to_asset
+from pysrc.util.types import Asset
+import pytest
+
+def test_asset_conversion() -> None:
+    for asset in Asset:
+        assert kraken_to_asset(asset_to_kraken(asset)) == asset
