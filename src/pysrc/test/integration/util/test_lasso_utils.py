af4f72ad0c50d808bdc14c63d28b84e5265b7aa5
Jiarui Cao <jerrycao030111@outlook.com>
2024-10-19 23:20:22 -0500

    base onto Aidan's branch

--- src/pysrc/test/integration/util/test_lasso_utils.py
+++ src/pysrc/test/integration/util/test_lasso_utils.py
@@ -4,11 +4,15 @@ from pysrc.test.helpers import get_resources_path
 from pathlib import Path
 
 
-def test_check_lasso_models() -> None:
+def test_check_lasso_models_positive() -> None:
     resource_path = get_resources_path(str(Path(__file__).resolve()))
-    assert check_lasso_models(resource_path, [])
-    assert check_lasso_models(resource_path, [Asset.BTC])
-    assert check_lasso_models(resource_path, [Asset.BTC, Asset.DOGE, Asset.ETH])
+    assert not check_lasso_models(resource_path, [Asset.ADA])
+    assert not check_lasso_models(resource_path, [Asset.ADA, Asset.AVAX, Asset.SHIB])
+    assert not check_lasso_models(resource_path, [Asset.BTC, Asset.ADA])
+
+
+def test_check_lasso_models_negative() -> None:
+    resource_path = get_resources_path(str(Path(__file__).resolve()))
     assert not check_lasso_models(resource_path, [Asset.ADA])
     assert not check_lasso_models(resource_path, [Asset.ADA, Asset.AVAX, Asset.SHIB])
     assert not check_lasso_models(resource_path, [Asset.BTC, Asset.ADA])
--- src/pysrc/util/lasso_utils.py
+++ src/pysrc/util/lasso_utils.py
@@ -1,8 +1,9 @@
 from pysrc.util.types import Asset
+from pysrc.util.enum_conversions import enum_to_string
 from pathlib import Path
 
 def get_lasso_model_filename(asset: Asset) -> str:
-    return(str(asset.name) + "_lasso.pkl")
+    return(enum_to_string(asset) + "_lasso.pkl")
 
 def check_lasso_models(directory_path: Path, assets: list[Asset]) -> bool:
     for asset in assets:
