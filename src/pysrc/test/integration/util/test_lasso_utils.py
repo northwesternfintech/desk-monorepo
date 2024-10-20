e92535e8990c12e13345d9468bb247da30617a9c
Jiarui Cao <jerrycao030111@outlook.com>
2024-10-19 23:47:58 -0500

    run formatter

--- src/pysrc/test/integration/util/test_lasso_utils.py
+++ src/pysrc/test/integration/util/test_lasso_utils.py
@@ -6,12 +6,16 @@ from pysrc.test.helpers import get_resources_path
 def test_check_lasso_models_positive() -> None:
     resource_path = get_resources_path(__file__)
     assert not search_model_in_directory(resource_path, [Asset.ADA])
-    assert not search_model_in_directory(resource_path, [Asset.ADA, Asset.AVAX, Asset.SHIB])
+    assert not search_model_in_directory(
+        resource_path, [Asset.ADA, Asset.AVAX, Asset.SHIB]
+    )
     assert not search_model_in_directory(resource_path, [Asset.BTC, Asset.ADA])
 
 
 def test_check_lasso_models_negative() -> None:
     resource_path = get_resources_path(__file__)
     assert not search_model_in_directory(resource_path, [Asset.ADA])
-    assert not search_model_in_directory(resource_path, [Asset.ADA, Asset.AVAX, Asset.SHIB])
+    assert not search_model_in_directory(
+        resource_path, [Asset.ADA, Asset.AVAX, Asset.SHIB]
+    )
     assert not search_model_in_directory(resource_path, [Asset.BTC, Asset.ADA])
--- src/pysrc/util/lasso_utils.py
+++ src/pysrc/util/lasso_utils.py
@@ -2,8 +2,10 @@ from pysrc.util.types import Asset
 from pysrc.util.enum_conversions import enum_to_string
 from pathlib import Path
 
+
 def asset_to_model_filename(asset: Asset) -> str:
-    return(f"{enum_to_string(asset)}_lasso.pkl")
+    return f"{enum_to_string(asset)}_lasso.pkl"
+
 
 def search_model_in_directory(directory_path: Path, assets: list[Asset]) -> bool:
     for asset in assets:
