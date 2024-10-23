d22697ffe1bc5dc7f1ff15c587379ad323dcc769
minnce <chrisminn205@gmail.com>
2024-10-23 16:54:47 -0500

    init func impls

---
+++ src/pysrc/adapters/kraken/future/containers.py
@@ -0,0 +1,11 @@
+from util import exceptions
+
+def safe_at(list, idx):
+    if idx < 0 or idx >= len(list):
+        return exceptions.DIE("Safe at failed due to index out of bounds")        
+    return list[idx]
+
+def get_single(list):
+    if len(list) != 1:
+        return exceptions.DIE("Get single failed due to list not having exactly one element")
+    return list[0]
