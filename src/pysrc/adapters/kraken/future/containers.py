9775ba26f8d3402ae13e0ba86645a7aff9d9dc70
Ryan Wong <rewong03@gmail.com>
2024-10-23 16:41:35 -0500

    testing some functions

--- src/pysrc/adapters/kraken/future/containers.py
+++ src/pysrc/adapters/kraken/future/containers.py
@@ -21,6 +21,9 @@ class OrderStatus(Enum):
     TRIGGER_ACTIVATION_FAILURE = 5
     UNTOUCHED = 6
     PARTIALLY_FILLED = 7
+    PLACED = 8
+    EDITED = 9
+    FILLED = 10
 
 class TriggerSignal(Enum):
     MARK = 0
@@ -30,20 +33,33 @@ class TriggerSignal(Enum):
 class Order:
     def __init__(
             self,
-            order_type: OrderType,
             symbol: str,
             side: OrderSide,
-            size: float,
-            limit_price: Optional[float],
-            stop_price: Optional[float],
-            order_id: Optional[str],
-            filled_size: Optional[float],
-            unfilled_size: Optional[float],
-            reduce_only: Optional[bool],
-            trigger_signal: Optional[TriggerSignal],
-            last_update_time: Optional[float]
+            size: Optional[float] = None,
+            order_type: Optional[OrderType] = None,
+            status: Optional[OrderStatus] = None,
+            limit_price: Optional[float] = None,
+            stop_price: Optional[float] = None,
+            order_id: Optional[str] = None,
+            filled_size: Optional[float] = None,
+            unfilled_size: Optional[float] = None,
+            reduce_only: Optional[bool] = None,
+            trigger_signal: Optional[TriggerSignal] = None,
+            last_update_time: Optional[float] = None
     ):
-        pass
+        self.symbol = symbol
+        self.side = side
+        self.size = size
+        self.order_type = order_type
+        self.status = status
+        self.limit_price = limit_price
+        self.stop_price = stop_price
+        self.order_id = order_id
+        self.filled_size = filled_size
+        self.unfilled_size = unfilled_size
+        self.reduce_only = reduce_only
+        self.trigger_signal = trigger_signal
+        self.last_update_time = last_update_time
 
 
 class PriceUnit(Enum):
@@ -54,13 +70,18 @@ class OrderRequest:
     def __init__(
             self,
             order: Order,
-            process_before: str,
-            trailing_stop_max_deviation: Optional[bool],
-            trailing_stop_deviation_unit: Optional[PriceUnit],
-            limit_price_offset_value: Optional[float],
-            limit_price_offset_unit: Optional[PriceUnit]
+            process_before: Optional[str] = None,
+            trailing_stop_max_deviation: Optional[bool] = None,
+            trailing_stop_deviation_unit: Optional[PriceUnit] = None,
+            limit_price_offset_value: Optional[float] = None,
+            limit_price_offset_unit: Optional[PriceUnit] = None
     ):
-        pass
+        self.order = order
+        self.process_before = process_before
+        self.trailing_stop_max_deviation = trailing_stop_max_deviation
+        self.trailing_stop_deviation_unit = trailing_stop_deviation_unit
+        self.limit_price_offset_value = limit_price_offset_value
+        self.limit_price_offset_unit = limit_price_offset_unit
 
 class PositionSide(Enum):
     LONG = 0
--- src/pysrc/adapters/kraken/future/utils.py
+++ src/pysrc/adapters/kraken/future/utils.py
@@ -1,11 +1,63 @@
-from pysrc.adapters.kraken.future.containers import PositionSide
+import urllib
+from pysrc.adapters.kraken.future.containers import OrderStatus, OrderType, PositionSide, PriceUnit, TriggerSignal
+from pysrc.util.types import OrderSide
 
 
 def str_to_position_side(s: str) -> PositionSide:
+    return PositionSide[s.upper()]
+
+def position_side_to_str(p: PositionSide) -> str:
+    return p.name.lower()
+        
+def str_to_order_type(s: str) -> OrderType:
+    return OrderType[s.upper()]
+        
+def order_type_to_str(o: OrderType) -> str:
+    return o.name.lower()
+        
+def str_to_order_status(s: str) -> OrderStatus:
+    return OrderStatus[s.upper()]
+
+def order_status_to_str(o: OrderStatus) -> str:
+    return o.name.lower()
+        
+def str_to_trigger_signal(s: str) -> TriggerSignal:
+    return TriggerSignal[s.upper()]
+
+def trigger_signal_to_str(t: TriggerSignal) -> str:
+    return t.name.lower()
+        
+def str_to_order_side(s: str) -> OrderSide:
     match s:
-        case "long":
-            return PositionSide.LONG
-        case "short":
-            return PositionSide.SHORT
+        case "buy":
+            return OrderSide.BID
+        case "sell":
+            return OrderSide.ASK
         case _:
-            raise ValueError(f"Can't convert '{s}' to PositionSide")
+            raise ValueError(f"Can't convert '{s}' to OrderSide")
+        
+def order_side_to_str(o: OrderSide) -> str:
+    match o:
+        case OrderSide.BID:
+            return "buy"
+        case OrderSide.ASK:
+            return "sell"
+        
+def str_to_price_unit(s: str) -> PriceUnit:
+    return PriceUnit[s.upper()]
+
+def price_unit_to_str(p: PriceUnit) -> str:
+    return p.name.lower()
+
+def url_encode_dict(d: dict[str]) -> str:
+    cleaned_dict = {}
+    for k, v in d.items():
+        if v is None:
+            continue
+
+        if type(v) is dict:
+            cleaned_dict[k] = url_encode_dict(v)
+        else:
+            cleaned_dict[k] = v
+
+    return urllib.parse.urlencode(cleaned_dict)
