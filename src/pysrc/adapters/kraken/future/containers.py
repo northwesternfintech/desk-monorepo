55e6452215add5ab88f76e7b884f7074ccbc488d
minnce <chrisminn205@gmail.com>
2024-10-25 14:40:55 -0500

    fixes to lint, stupid error

--- src/pysrc/adapters/kraken/future/containers.py
+++ src/pysrc/adapters/kraken/future/containers.py
@@ -1,12 +1,16 @@
 from pysrc.util import exceptions
 from typing import Any, List, Optional
 
-def safe_at(list : List[Any], idx: int) -> Optional[Any]:
+
+def safe_at(list: List[Any], idx: int) -> Optional[Any]:
     if (idx < 0 and abs(idx) > len(list)) or (idx >= 0 and idx >= len(list)):
-        return exceptions.DIE("Safe at failed due to index out of bounds")        
+        return exceptions.DIE("Safe at failed due to index out of bounds")
     return list[idx]
 
-def get_single(list : List[Any]) -> Optional[Any]:
+
+def get_single(list: List[Any]) -> Optional[Any]:
     if len(list) != 1:
-        return exceptions.DIE("Get single failed due to list not having exactly one element")
+        return exceptions.DIE(
+            "Get single failed due to list not having exactly one element"
+        )
     return list[0]
--- src/pysrc/test/unit/adapters/kraken/future/test_kraken_containers.py
+++ src/pysrc/test/unit/adapters/kraken/future/test_kraken_containers.py
@@ -3,34 +3,40 @@ from pysrc.util import exceptions
 import pytest
 
 
-def test_safe_at() -> int:
-    testList = [1,2,3]
-    assert(safe_at(testList, 0) == 1)
-    assert(safe_at(testList, 1) == 2)
-    assert(safe_at(testList, 2) == 3)
-    assert(safe_at(testList, -1) == 3)
-    assert(safe_at(testList, -2) == 2)
-    assert(safe_at(testList, -3) == 1)
+def test_safe_at() -> None:
+    testList = [1, 2, 3]
+    assert safe_at(testList, 0) == 1
+    assert safe_at(testList, 1) == 2
+    assert safe_at(testList, 2) == 3
+    assert safe_at(testList, -1) == 3
+    assert safe_at(testList, -2) == 2
+    assert safe_at(testList, -3) == 1
 
-    with pytest.raises(AssertionError, match="Safe at failed due to index out of bounds"):
+    with pytest.raises(
+        AssertionError, match="Safe at failed due to index out of bounds"
+    ):
         safe_at(testList, 3)
 
-    with pytest.raises(AssertionError, match="Safe at failed due to index out of bounds"):
+    with pytest.raises(
+        AssertionError, match="Safe at failed due to index out of bounds"
+    ):
         safe_at(testList, -4)
 
-    return 0
-
-def test_get_single() -> int:
+def test_get_single() -> None:
     testSingleFailList: list[int] = []
-    testSingleFailList2 = [1,2]
+    testSingleFailList2 = [1, 2]
     singleList = [1]
 
-    assert(get_single(singleList) == 1)
+    assert get_single(singleList) == 1
 
-    with pytest.raises(AssertionError, match="Get single failed due to list not having exactly one element"):
+    with pytest.raises(
+        AssertionError,
+        match="Get single failed due to list not having exactly one element",
+    ):
         get_single(testSingleFailList)
 
-    with pytest.raises(AssertionError, match="Get single failed due to list not having exactly one element"):
+    with pytest.raises(
+        AssertionError,
+        match="Get single failed due to list not having exactly one element",
+    ):
         get_single(testSingleFailList2)
-
-    return 0
