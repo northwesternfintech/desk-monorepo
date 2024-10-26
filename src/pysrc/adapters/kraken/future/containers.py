8d2b95f2fb0479a9e4be9c00518538423563d430
minnce <chrisminn205@gmail.com>
2024-10-26 14:24:16 -0500

    fixed comments

--- src/pysrc/adapters/kraken/future/containers.py
+++ src/pysrc/adapters/kraken/future/containers.py
@@ -1,15 +1,14 @@
 from pysrc.util.exceptions import DIE
 from collections.abc import Sequence
-from typing import Any
 
 
-def safe_at(list: Sequence[Any], idx: int) -> Any:
-    if (idx < 0 and abs(idx) > len(list)) or (idx >= 0 and idx >= len(list)):
+def safe_at[T](lst: Sequence[T], idx: int) -> T:
+    if (idx < 0 and abs(idx) > len(lst)) or (idx >= 0 and idx >= len(lst)):
         return DIE("Safe at failed due to index out of bounds")
-    return list[idx]
+    return lst[idx]
 
 
-def get_single(list: Sequence[Any]) -> Any:
-    if len(list) != 1:
+def get_single[T](singleList: Sequence[T]) -> T:
+    if len(singleList) != 1:
         return DIE("Get single failed due to list not having exactly one element")
-    return list[0]
+    return singleList[0]
