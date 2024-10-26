65949af5add2eff278bfb7d13ac7127ca3da38f3
minnce <chrisminn205@gmail.com>
2024-10-25 20:18:40 -0500

    Actually no need for none return label on safe at and get single

--- src/pysrc/adapters/kraken/future/containers.py
+++ src/pysrc/adapters/kraken/future/containers.py
@@ -3,13 +3,13 @@ from collections.abc import Sequence
 from typing import Any
 
 
-def safe_at(list: Sequence[Any], idx: int) -> Any | None:
+def safe_at(list: Sequence[Any], idx: int) -> Any:
     if (idx < 0 and abs(idx) > len(list)) or (idx >= 0 and idx >= len(list)):
         return DIE("Safe at failed due to index out of bounds")
     return list[idx]
 
 
-def get_single(list: Sequence[Any]) -> Any | None:
+def get_single(list: Sequence[Any]) -> Any:
     if len(list) != 1:
         return DIE("Get single failed due to list not having exactly one element")
     return list[0]
