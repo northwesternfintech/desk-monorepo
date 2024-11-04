4d08f2f9f528307efc6121a30dac8df1586d33a3
Ryan Wong <rewong03@gmail.com>
2024-11-04 01:37:42 -0600

    removing local test files

--- src/pysrc/test_f.py
+++
@@ -1,7 +0,0 @@
-from pysrc.adapters.kraken.historical.historical_data_client import HistoricalDataClient
-
-if __name__ == "__main__":
-    client = HistoricalDataClient(
-        drive_auth_token="ya29.a0AeDClZDMyTKeZBjsIz_aogu9FMSO5MtLRezNy2Vnstdh2XHhQH1Tna3iwD5xel1tAMSiQPrUAtXAqFxN-GTKTNGGhOwa11w7ttrGQE3capd5J0pqCAlXoRNM1_hAMu3Zf8m00fn_JWdDo3aVMYB0geVcI2v_Ve0hvIjCR2h0owaCgYKARkSARESFQHGX2Mit8dXIe8tZbVLu8gAssqZfg0177"
-    )
-    client.download("/Users/ryan/Downloads/test_3")
