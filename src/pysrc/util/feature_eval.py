c8bef3cdd8faf4a6e98b7f5fe23d8a0a1c5d9fc4
kyeoul <insertwinterhere@gmail.com>
2024-11-07 23:30:04 -0600

    feature evaluation stubbing

--- src/pysrc/util/feature_eval.py
+++ src/pysrc/util/feature_eval.py
@@ -1,14 +1,15 @@
 from typing import Any, Optional
 
+
 class Evaluator:
+
     def __init__(self, features, coin, start, end):
-        # 
         self.features = features
         self.coin = coin
         self.start = start
         self.end = end
-        pass 
-    
+        pass
+
     def _get_data_daterange(start, end):
         # hist_client = HistoricalDataClient()
         # res = []
@@ -23,3 +24,9 @@ class Evaluator:
         # feature_dict = generator_client.on_tick(..., trades)
         # ^ how to get snapshots? HistoricalDataClient() only has methods for trades
         pass
+
+    def evaluate_features(self, target):
+        # calc_features = self.calculate_features()
+        # assess against a target...?
+        pass
+
