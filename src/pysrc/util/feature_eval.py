b6c6b97587550692fc6e317a8d9116930fa0b6a3
kyeoul <insertwinterhere@gmail.com>
2024-11-11 17:24:50 -0600

    vague implementations, some things are still weird

--- src/pysrc/util/feature_eval.py
+++ src/pysrc/util/feature_eval.py
@@ -1,32 +1,42 @@
-from typing import Any, Optional
+from pysrc.adapters.kraken.historical.historical_data_client import HistoricalDataClient
+from datetime import datetime
+from dateutil.rrule import rrule, DAILY
+from pysrc.util.types import TradeMessage, Asset
+from pysrc.signal.base_feature_generator import BaseFeatureGenerator
+import numpy as np
 
 
 class Evaluator:
 
-    def __init__(self, features, coin, start, end):
+    def __init__(self, features: list[str], asset: Asset, start: datetime, end: datetime):
         self.features = features
-        self.coin = coin
+        self.asset = asset
         self.start = start
         self.end = end
         pass
 
-    def _get_data_daterange(start, end):
-        # hist_client = HistoricalDataClient()
-        # res = []
-        # for date in range(start, end):
-        #   res.append(hist_client.get_trades(self.coin, date, resource_path...?))
-        # return res
-        pass
+    def _get_data_daterange(self, start: datetime, end: datetime) -> list[TradeMessage]:
+        hist_client = HistoricalDataClient()
+        res = []
+        for date in rrule(DAILY, dtstart=start, until=end):
+            res.extend(hist_client.get_trades(self.coin, date, ""))
+        return res
 
-    def calculate_features(self):
-        # trades = self._get_data_daterange(self.start, self.end)
-        # generator_client = BaseFeatureGenerator()
-        # feature_dict = generator_client.on_tick(..., trades)
-        # ^ how to get snapshots? HistoricalDataClient() only has methods for trades
-        pass
+    def calculate_features(self) -> dict[str, list[float]]:
+        trades = self._get_data_daterange(self.start, self.end)
+        # ^ this returns a list[TradeMessage], but on_tick expects dict[str, TradeMessage]
+        # What is key?
+        snapshots = {}
+        # ^ how to get snapshots?
+        generator_client = BaseFeatureGenerator()
+        feature_dict = generator_client.on_tick(snapshots, trades)
+        return feature_dict[self.asset]
 
-    def evaluate_features(self, target):
-        # calc_features = self.calculate_features()
-        # assess against a target...?
-        pass
-
+    # Suppose that returns is passed to evaluate_features
+    def evaluate_features(self, target: list[float]):
+        calc_features = self.calculate_features()
+        input_matrix = []
+        for feature in self.features:
+            input_matrix.append(calc_features[feature])
+        input_matrix.append(target)
+        return np.corrocoef(input_matrix)
