87705b9588d5ed7c6449b77610460c046d6b8ae7
Ethan Havemann <echavemann@gmail.com>
2024-11-14 16:35:15 -0600

    add unit

--- src/pysrc/util/system.py
+++ src/pysrc/util/system.py
@@ -1,6 +1,7 @@
 import socket
 import getpass
 from pysrc.util.types import Server
+from pysrc.util.exceptions import DIE
 
 SERVER_USERNAME_TO_SLACK_NAME: dict[str, str] = {
     "echavemann": "Ethan Havemann",
@@ -33,6 +34,15 @@ def get_current_server() -> Server:
 
 def get_current_user() -> str:
     return getpass.getuser()
+
+
+def get_current_user_slack_name() -> str:
+    current_user: str = get_current_user()
+    print(current_user)
+    slack_name = SERVER_USERNAME_TO_SLACK_NAME.get(current_user)
+    if slack_name is None:
+        DIE(f"Could not find user {current_user} in the username to slack name mapping!")
+    return slack_name
 
 
 def _get_current_server_hostname() -> str:
