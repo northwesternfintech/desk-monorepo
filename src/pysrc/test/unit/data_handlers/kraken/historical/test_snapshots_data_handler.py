b166b0700fcd393d4575a67e17809e842bc2955f
Jiarui Cao <jerrycao030111@outlook.com>
2024-11-19 18:52:36 -0600

    redesign write algorithm

--- src/pysrc/test/unit/data_handlers/kraken/historical/test_snapshots_data_handler.py
+++ src/pysrc/test/unit/data_handlers/kraken/historical/test_snapshots_data_handler.py
@@ -6,7 +6,7 @@ from pysrc.data_handlers.kraken.historical.snapshots_data_handler import (
     SnapshotsDataHandler,
 )
 from pysrc.adapters.messages import SnapshotMessage
-from pysrc.util.types import Market, Asset
+from pysrc.util.types import Market
 
 random.seed(42)
 resource_path = get_resources_path(__file__)
