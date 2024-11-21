254cd74f58ebe8a57931eb00801483fe0948068e
Aidan Villasenor <aidan.villasenor@gmail.com>
2024-11-20 21:24:13 -0600

    retry logic

--- src/pysrc/util/websocket.py
+++ src/pysrc/util/websocket.py
@@ -7,8 +7,12 @@ from typing import Optional
 
 
 class WebSocketClient(ABC):
-    def __init__(self, base_url: str) -> None:
+    def __init__(
+        self, base_url: str, max_retries: int = 5, retry_delay: int = 5
+    ) -> None:
         self.base_url = base_url
+        self.max_retries = max_retries  # -1 for infinite
+        self.retry_delay = retry_delay
         self.ws: Optional[ClientConnection] = None
         self._listener_task: Optional[asyncio.Task] = None
 
@@ -17,23 +21,36 @@ class WebSocketClient(ABC):
         self._listener_task = loop.create_task(self._connect_and_listen())
 
     async def _connect_and_listen(self) -> None:
-        try:
-            print(f"Connecting to WebSocket at {self.base_url}...")
-            self.ws = await asyncio.wait_for(
-                websockets.connect(self.base_url), timeout=10
-            )
-            print("Connected to WebSocket.")
+        retries = 0
+        while self.max_retries == -1 or retries < self.max_retries:
+            try:
+                print(
+                    f"Connecting to WebSocket at {self.base_url} (Attempt {retries + 1})..."
+                )
+                self.ws = await asyncio.wait_for(
+                    websockets.connect(self.base_url), timeout=10
+                )
+                print("Connected to WebSocket.")
 
-            await self.on_connect()
-            await self._listen_for_messages()
-        except asyncio.TimeoutError:
-            print("Connection attempt timed out.")
-        except websockets.ConnectionClosed as e:
-            print(f"WebSocket connection closed: {e}")
-        except Exception as e:
-            print(f"Unexpected error: {e}")
-        finally:
-            await self.on_disconnect()
+                await self.on_connect()
+
+                await self._listen_for_messages()
+                return
+            except asyncio.TimeoutError:
+                print("Connection attempt timed out.")
+            except websockets.ConnectionClosed as e:
+                print(f"WebSocket connection closed: {e}")
+            except Exception as e:
+                print(f"Unexpected error: {e}")
+            finally:
+                await self.on_disconnect()
+
+            retries += 1
+            if self.max_retries != -1 and retries >= self.max_retries:
+                print("Max retries reached. Shutting websocket down.")
+                break
+            print(f"Retrying in {self.retry_delay} seconds...")
+            await asyncio.sleep(self.retry_delay)
 
     async def _listen_for_messages(self) -> None:
         try:
