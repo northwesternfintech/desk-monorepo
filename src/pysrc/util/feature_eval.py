9661015483167c3748ab60166eeeb2ad9260f436
kyeoul <insertwinterhere@gmail.com>
2024-11-22 09:44:22 -0600

    final implementation, moving to unit tests

--- src/pysrc/util/feature_eval.py
+++ src/pysrc/util/feature_eval.py
@@ -1,17 +1,19 @@
-from pysrc.adapters.kraken.historical.trades.historical_trades_data_client import (
-    HistoricalTradesDataClient,
-)
-from pysrc.adapters.kraken.historical.updates.historical_updates_data_client import (
-    HistoricalUpdatesDataClient,
-)
-
+import itertools
+import os
 from datetime import datetime
-from dateutil.rrule import rrule, DAILY
-from pysrc.util.types import Asset
-from pysrc.adapters.messages import TradeMessage, SnapshotMessage
-from pysrc.signal.base_feature_generator import BaseFeatureGenerator
+from pathlib import Path
+
 import numpy as np
+from dateutil.rrule import DAILY, rrule
+
+from pysrc.adapters.messages import SnapshotMessage, TradeMessage
+from pysrc.data_handlers.kraken.historical.snapshots_data_handler import (
+    SnapshotsDataHandler,
+)
+from pysrc.data_handlers.kraken.historical.trades_data_handler import TradesDataHandler
+from pysrc.signal.base_feature_generator import BaseFeatureGenerator
 from pysrc.util.enum_conversions import string_to_enum
+from pysrc.util.types import Asset
 
 
 class Evaluator:
@@ -23,25 +25,49 @@ class Evaluator:
         pass
 
     def _get_data_daterange(
-        self, start: datetime, end: datetime
+        self,
+        start: datetime,
+        end: datetime,
+        trades_resource_path: str,
+        snapshots_resource_path: str,
     ) -> zip[tuple[TradeMessage, SnapshotMessage]]:
-        hist_client = HistoricalTradesDataClient()
-        res = []
-        for date in rrule(DAILY, dtstart=start, until=end):
-            res.extend(hist_client.get_trades(self.asset, date, ""))
+        trades_client = TradesDataHandler()
+        snapshots_client = SnapshotsDataHandler()
 
-        update_client = HistoricalUpdatesDataClient("")
-        generator = update_client.stream_updates(self.asset, start, end)
-        return zip(res, generator)
+        trade_generators = []
+        snapshot_generators = []
+
+        for date in rrule(DAILY, dtstart=start, until=end):
+            trades_filepath = os.path.join(
+                trades_resource_path, self.asset, date.strftime("%m_%d_%Y") + ".bin"
+            )
+            trade_gen = trades_client.stream_read(Path(trades_filepath))
+
+            snapshots_filepath = os.path.join(
+                snapshots_resource_path, self.asset, date.strftime("%m_%d_%Y") + ".bin"
+            )
+            snapshot_gen = snapshots_client.stream_read(Path(snapshots_filepath))
+
+            trade_generators.append(trade_gen)
+            snapshot_generators.append(snapshot_gen)
+
+        combined_trades_gen = itertools.chain.from_iterable(trade_generators)
+        combined_snapshots_gen = itertools.chain.from_iterable(snapshot_generators)
+        return zip(combined_trades_gen, combined_snapshots_gen)
 
     def calculate_features(
-        self, generator_client: BaseFeatureGenerator
+        self,
+        generator_client: BaseFeatureGenerator,
+        trades_filepath: str,
+        snapshots_filepath: str,
     ) -> dict[str, list[float]]:
-        data = self._get_data_daterange(self.start, self.end)
+        data = self._get_data_daterange(
+            self.start, self.end, trades_filepath, snapshots_filepath
+        )
         feature_dict: dict[str, list[float]] = dict.fromkeys(self.features, [])
         for trade, snapshot in data:
             features = generator_client.on_tick(
-                {self.asset: snapshot}, {self.asset: trade}
+                {self.asset: snapshot}, {self.asset: [trade]}
             )
             for feature in self.features:
                 asset_enum = string_to_enum(Asset, self.asset)
