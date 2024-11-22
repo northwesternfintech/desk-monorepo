64e9431e6a30cc39a7cde7176b4a9938643bdf9b
John Hileman <106003582+jhileman07@users.noreply.github.com>
2024-11-21 21:50:29 -0600

    setting up config

---
+++ src/pysrc/logger/logging_config.py
@@ -0,0 +1,30 @@
+import logging
+import logging.config
+
+def setup_logging():
+    logging.config.dictConfig({
+        "version": 1,
+        "disable_existing_loggers": False,
+        "formatters": {
+            "standard": {
+                "format": "%(asctime)s [%(levelname)s] %(name)s: %(message)s"
+            }
+        },
+        "handlers": {
+            "console": {
+                "class": "logging.StreamHandler",
+                "formatter": "standard",
+                "level": "INFO"
+            },
+            "file": {
+                "class": "logging.FileHandler",
+                "filename": "src/pysrc/logger/logs/project.log",
+                "formatter": "standard",
+                "level": "DEBUG"
+            }
+        },
+        "root": {
+            "handlers": ["console", "file"],
+            "level": "DEBUG",
+        },
+    })
