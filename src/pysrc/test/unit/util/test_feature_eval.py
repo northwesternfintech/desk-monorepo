2be86a14e93d2f88af1de99088c76131ed587974
kyeoul <insertwinterhere@gmail.com>
2024-12-02 22:19:17 -0600

    more solid feature eval

--- src/pysrc/test/unit/util/test_feature_eval.py
+++ src/pysrc/test/unit/util/test_feature_eval.py
@@ -1,13 +1,12 @@
 import os
 from datetime import datetime
-from pathlib import Path
 
 import pytest
 from pyzstd import CParameter, compress
 
 from pysrc.adapters.messages import SnapshotMessage, TradeMessage
 from pysrc.signal.base_feature_generator import BaseFeatureGenerator
-from pysrc.test.feature_eval_helpers import TestFeatureGenerator
+from pysrc.signal.example_kraken_feature_generator import ExampleKrakenFeatureGenerator
 from pysrc.test.helpers import get_resources_path
 from pysrc.util.feature_eval import Evaluator
 from pysrc.util.types import Asset, Market, OrderSide
@@ -17,11 +16,11 @@ resource_path = get_resources_path(__file__)
 
 @pytest.fixture
 def client() -> Evaluator:
-    test_features = ["a", "b", "c"]
+    test_features = ["open", "high", "low", "close"]
     asset = Asset.BTC
     market = Market.KRAKEN_SPOT
-    start = datetime(year=2024, month=11, day=22)
-    end = datetime(year=2024, month=11, day=22)
+    start = datetime(year=2024, month=6, day=25)
+    end = datetime(year=2024, month=6, day=26)
     return Evaluator(
         features=test_features,
         asset=asset,
@@ -34,7 +33,7 @@ def client() -> Evaluator:
 
 @pytest.fixture
 def feature_gen() -> BaseFeatureGenerator:
-    return TestFeatureGenerator()
+    return ExampleKrakenFeatureGenerator()
 
 
 def test_feature_calculation(
@@ -42,23 +41,30 @@ def test_feature_calculation(
 ) -> None:
     snapshots = [
         SnapshotMessage(
-            time=1,
+            time=1719273600,
             feedcode="XXBTZUSD",
-            market=Market.KRAKEN_USD_FUTURE,
+            market=Market.KRAKEN_SPOT,
             bids=[],
             asks=[[1.0, 2.0]],
         ),
         SnapshotMessage(
-            time=2,
+            time=1719273600,
             feedcode="XXBTZUSD",
-            market=Market.KRAKEN_USD_FUTURE,
+            market=Market.KRAKEN_SPOT,
             bids=[[3.0, 4.0], [68717.5, -3000.0]],
             asks=[[1.0, 2.0], [68717.5, -3000.0]],
         ),
         SnapshotMessage(
-            time=3,
+            time=1719273601,
             feedcode="XXBTZUSD",
-            market=Market.KRAKEN_USD_FUTURE,
+            market=Market.KRAKEN_SPOT,
+            bids=[[3.0, 4.0], [68717.5, -3000.0], [7.0, 8.0]],
+            asks=[[1.0, 2.0], [68717.5, -3000.0]],
+        ),
+        SnapshotMessage(
+            time=1719273602,
+            feedcode="XXBTZUSD",
+            market=Market.KRAKEN_SPOT,
             bids=[[3.0, 4.0], [68717.5, -3000.0], [7.0, 8.0]],
             asks=[[1.0, 2.0], [68717.5, -3000.0]],
         ),
@@ -70,7 +76,7 @@ def test_feature_calculation(
 
     test_dir_snapshots = resource_path / "snapshots" / "XXBTZUSD"
     os.makedirs(test_dir_snapshots, exist_ok=True)
-    test_path = test_dir_snapshots / "11_22_2024.bin"
+    test_path = test_dir_snapshots / "06_25_2024.bin"
 
     with open(test_path, "wb") as f:
         f.write(
@@ -79,13 +85,16 @@ def test_feature_calculation(
 
     trades = [
         TradeMessage(
-            1, "XXBTZUSD", 1, 10.0, 1.0, OrderSide.BID, Market.KRAKEN_USD_FUTURE
+            1719273600, "XXBTZUSD", 1, 10.0, 1.0, OrderSide.BID, Market.KRAKEN_SPOT
         ),
         TradeMessage(
-            2, "XXBTZUSD", 1, 10.02, 0.5, OrderSide.BID, Market.KRAKEN_USD_FUTURE
+            1719273600, "XXBTZUSD", 1, 10.02, 0.5, OrderSide.BID, Market.KRAKEN_SPOT
         ),
         TradeMessage(
-            3, "XXBTZUSD", 1, 9.99, 1.5, OrderSide.BID, Market.KRAKEN_USD_FUTURE
+            1719273601, "XXBTZUSD", 1, 9.9, 1.5, OrderSide.BID, Market.KRAKEN_SPOT
+        ),
+        TradeMessage(
+            1719273602, "XXBTZUSD", 1, 9.0, 1.5, OrderSide.BID, Market.KRAKEN_SPOT
         ),
     ]
 
@@ -95,7 +104,7 @@ def test_feature_calculation(
 
     test_dir_snapshots = resource_path / "trades" / "XXBTZUSD"
     os.makedirs(test_dir_snapshots, exist_ok=True)
-    test_path = test_dir_snapshots / "11_22_2024.bin"
+    test_path = test_dir_snapshots / "06_25_2024.bin"
 
     with open(test_path, "wb") as f:
         f.write(
@@ -103,15 +112,42 @@ def test_feature_calculation(
         )
 
     result = client.calculate_features(feature_gen)
-    assert result["a"] == [7.0, 7.0, 7.0]
-    assert result["b"] == [7.0, 7.0, 7.0]
-    assert result["c"] == [7.0, 7.0, 7.0]
+    assert result["open"][0] == pytest.approx(10.0, rel=1e-7)
+    assert result["open"][1] == pytest.approx(9.9, rel=1e-7)
+    assert result["open"][2] == pytest.approx(9.0, rel=1e-7)
+
+    assert result["high"][0] == pytest.approx(10.02, rel=1e-7)
+    assert result["high"][1] == pytest.approx(9.9, rel=1e-7)
+    assert result["high"][2] == pytest.approx(9.0, rel=1e-7)
+
+    assert result["low"][0] == pytest.approx(10.0, rel=1e-7)
+    assert result["low"][1] == pytest.approx(9.9, rel=1e-7)
+    assert result["low"][2] == pytest.approx(9.0, rel=1e-7)
+
+    assert result["close"][0] == pytest.approx(10.02, rel=1e-7)
+    assert result["close"][1] == pytest.approx(9.9, rel=1e-7)
+    assert result["close"][2] == pytest.approx(9.0, rel=1e-7)
 
 
 def test_feature_evaluation(client: Evaluator) -> None:
-    features = {"a": [1.0, 2.0, 3.0], "b": [2.0, 4.0, 6.0], "c": [3.0, 6.0, 9.0]}
+    features = {
+        "open": [1.0, 2.0, 3.0],
+        "high": [2.0, 4.0, 6.0],
+        "low": [-1.0, -2.0, -3.0],
+        "close": [1.0, 2.0, 3.0],
+    }
 
     target = [4.0, 8.0, 12.0]
     result = client.evaluate_features(features, target)
 
     assert result is not None
+
+    expected_result = [
+        [1, 1, -1, 1, 1],
+        [1, 1, -1, 1, 1],
+        [-1, -1, 1, -1, -1],
+        [1, 1, -1, 1, 1],
+        [1, 1, -1, 1, 1],
+    ]
+
+    assert (result == expected_result).any()
--- src/pysrc/util/feature_eval.py
+++ src/pysrc/util/feature_eval.py
@@ -1,16 +1,11 @@
-import itertools
-from datetime import datetime, timedelta
+from datetime import datetime
 from pathlib import Path
-from typing import Iterable
 
 import numpy as np
 
 from pysrc.adapters.kraken.asset_mappings import asset_to_kraken
-from pysrc.adapters.messages import SnapshotMessage, TradeMessage
-from pysrc.data_handlers.kraken.historical.snapshots_data_handler import (
-    SnapshotsDataHandler,
-)
-from pysrc.data_handlers.kraken.historical.trades_data_handler import TradesDataHandler
+from pysrc.data_loaders.tick_snapshots_data_loader import TickSnapshotsDataLoader
+from pysrc.data_loaders.tick_trades_data_loader import TickTradesDataLoader
 from pysrc.signal.base_feature_generator import BaseFeatureGenerator
 from pysrc.util.types import Asset, Market
 
@@ -32,49 +27,16 @@ class Evaluator:
         self._market = market
         self._resource_path = resource_path
 
-    def _get_data_daterange(
-        self,
-        start: datetime,
-        end: datetime,
-    ) -> Iterable[tuple[TradeMessage, SnapshotMessage]]:
-        trades_client = TradesDataHandler()
-        snapshots_client = SnapshotsDataHandler()
-
-        trade_generators = []
-        snapshot_generators = []
-        delta_time = timedelta(days=1)
-        asset_str = asset_to_kraken(self._asset, self._market)
-
-        while start <= end:
-            trades_filepath = (
-                self._resource_path
-                / "trades"
-                / asset_str
-                / (start.strftime("%m_%d_%Y") + ".bin")
-            )
-            trade_gen = trades_client.stream_read(trades_filepath)
-
-            snapshots_filepath = (
-                self._resource_path
-                / "snapshots"
-                / asset_str
-                / (start.strftime("%m_%d_%Y") + ".bin")
-            )
-            snapshot_gen = snapshots_client.stream_read(snapshots_filepath)
-
-            trade_generators.append(trade_gen)
-            snapshot_generators.append(snapshot_gen)
-            start += delta_time
-
-        combined_trades_gen = itertools.chain.from_iterable(trade_generators)
-        combined_snapshots_gen = itertools.chain.from_iterable(snapshot_generators)
-        return zip(combined_trades_gen, combined_snapshots_gen)
-
     def calculate_features(
         self,
         generator_client: BaseFeatureGenerator,
     ) -> dict[str, list[float]]:
-        data = self._get_data_daterange(self._start, self._end)
+        trades_client = TickTradesDataLoader(
+            self._resource_path, self._asset, self._market, self._start, self._end
+        )
+        snapshots_client = TickSnapshotsDataLoader(
+            self._resource_path, self._asset, self._market, self._start, self._end
+        )
 
         feature_dict: dict[str, list[float]] = {}
         for feature in self._features:
@@ -82,12 +44,15 @@ class Evaluator:
 
         asset_str = asset_to_kraken(self._asset, self._market)
 
-        for trade, snapshot in data:
+        while (trade := trades_client.next()) is not None and (
+            snapshot := snapshots_client.next()
+        ) is not None:
             calc_features = generator_client.on_tick(
-                {asset_str: snapshot}, {asset_str: [trade]}
+                {asset_str: snapshot}, {asset_str: trade}
             )
-            for feature in self._features:
-                feature_dict[feature].extend(calc_features[self._asset][feature])
+            for i in range(len(self._features)):
+                feature = self._features[i]
+                feature_dict[feature].append(calc_features[self._asset]["features"][i])
         return feature_dict
 
     def evaluate_features(
